2f1abc818cb20d46fca13325f9b8750397153569
Richard Mackey <mackeyrj@gmail.com>
2025-05-24 22:38:54 -0400

    Updated Edit Trip

--- frmEditTrip.Designer.vb
+++ frmEditTrip.Designer.vb
@@ -33,126 +33,194 @@ Partial Class frmEditTrip
         Label4 = New Label()
         btnCancel = New Button()
         btnDeleteTrip = New Button()
-        TextBox1 = New TextBox()
+        txtTraveler1 = New TextBox()
+        txtTraveler1Init = New TextBox()
+        lblTraveler1 = New Label()
+        lblTraverl1Ininit = New Label()
+        lblTraveler2Ininit = New Label()
+        lblTraveler1Ininit = New Label()
+        txtTraveler2Init = New TextBox()
+        txtTraveler2 = New TextBox()
         SuspendLayout()
         ' 
         ' cbTripName
         ' 
         cbTripName.FormattingEnabled = True
-        cbTripName.Location = New Point(162, 64)
-        cbTripName.Margin = New Padding(2)
+        cbTripName.Location = New Point(275, 105)
         cbTripName.Name = "cbTripName"
-        cbTripName.Size = New Size(358, 33)
+        cbTripName.Size = New Size(606, 49)
         cbTripName.TabIndex = 0
         ' 
         ' btnEdit
         ' 
-        btnEdit.Location = New Point(56, 404)
-        btnEdit.Margin = New Padding(2)
+        btnEdit.Location = New Point(95, 663)
         btnEdit.Name = "btnEdit"
-        btnEdit.Size = New Size(111, 35)
+        btnEdit.Size = New Size(189, 57)
         btnEdit.TabIndex = 1
         btnEdit.Text = "Edit"
         btnEdit.UseVisualStyleBackColor = True
         ' 
         ' txtTripDate
         ' 
-        txtTripDate.Location = New Point(162, 140)
-        txtTripDate.Margin = New Padding(2)
+        txtTripDate.Location = New Point(275, 230)
         txtTripDate.Name = "txtTripDate"
-        txtTripDate.Size = New Size(358, 31)
+        txtTripDate.Size = New Size(606, 47)
         txtTripDate.TabIndex = 3
         ' 
         ' txtTripDestination
         ' 
-        txtTripDestination.Location = New Point(162, 212)
-        txtTripDestination.Margin = New Padding(2)
+        txtTripDestination.Location = New Point(275, 348)
         txtTripDestination.Name = "txtTripDestination"
-        txtTripDestination.Size = New Size(358, 31)
+        txtTripDestination.Size = New Size(606, 47)
         txtTripDestination.TabIndex = 4
         ' 
         ' txtTripDetails
         ' 
-        txtTripDetails.Location = New Point(162, 283)
-        txtTripDetails.Margin = New Padding(2)
+        txtTripDetails.Location = New Point(275, 464)
         txtTripDetails.Multiline = True
         txtTripDetails.Name = "txtTripDetails"
-        txtTripDetails.Size = New Size(358, 96)
+        txtTripDetails.Size = New Size(606, 155)
         txtTripDetails.TabIndex = 5
         ' 
         ' jlblTripName
         ' 
         jlblTripName.AutoSize = True
-        jlblTripName.Location = New Point(62, 69)
-        jlblTripName.Margin = New Padding(2, 0, 2, 0)
+        jlblTripName.Location = New Point(105, 113)
         jlblTripName.Name = "jlblTripName"
-        jlblTripName.Size = New Size(59, 25)
+        jlblTripName.Size = New Size(97, 41)
         jlblTripName.TabIndex = 6
         jlblTripName.Text = "Name"
         ' 
         ' Label2
         ' 
         Label2.AutoSize = True
-        Label2.Location = New Point(72, 143)
-        Label2.Margin = New Padding(2, 0, 2, 0)
+        Label2.Location = New Point(122, 235)
         Label2.Name = "Label2"
-        Label2.Size = New Size(49, 25)
+        Label2.Size = New Size(80, 41)
         Label2.TabIndex = 7
         Label2.Text = "Date"
         ' 
         ' Label3
         ' 
         Label3.AutoSize = True
-        Label3.Location = New Point(19, 215)
-        Label3.Margin = New Padding(2, 0, 2, 0)
+        Label3.Location = New Point(32, 353)
         Label3.Name = "Label3"
-        Label3.Size = New Size(102, 25)
+        Label3.Size = New Size(169, 41)
         Label3.TabIndex = 8
         Label3.Text = "Destination"
         ' 
         ' Label4
         ' 
         Label4.AutoSize = True
-        Label4.Location = New Point(56, 283)
-        Label4.Margin = New Padding(2, 0, 2, 0)
+        Label4.Location = New Point(95, 464)
         Label4.Name = "Label4"
-        Label4.Size = New Size(65, 25)
+        Label4.Size = New Size(107, 41)
         Label4.TabIndex = 9
         Label4.Text = "Details"
         ' 
         ' btnCancel
         ' 
-        btnCancel.Location = New Point(225, 404)
-        btnCancel.Margin = New Padding(2)
+        btnCancel.Location = New Point(382, 663)
         btnCancel.Name = "btnCancel"
-        btnCancel.Size = New Size(111, 35)
+        btnCancel.Size = New Size(189, 57)
         btnCancel.TabIndex = 10
         btnCancel.Text = "Close"
         btnCancel.UseVisualStyleBackColor = True
         ' 
         ' btnDeleteTrip
         ' 
-        btnDeleteTrip.Location = New Point(430, 413)
-        btnDeleteTrip.Margin = New Padding(2)
+        btnDeleteTrip.Location = New Point(731, 677)
         btnDeleteTrip.Name = "btnDeleteTrip"
-        btnDeleteTrip.Size = New Size(111, 35)
+        btnDeleteTrip.Size = New Size(189, 57)
         btnDeleteTrip.TabIndex = 11
         btnDeleteTrip.Text = "Delete"
         btnDeleteTrip.UseVisualStyleBackColor = True
         ' 
-        ' TextBox1
+        ' txtTraveler1
         ' 
-        TextBox1.Location = New Point(808, 54)
-        TextBox1.Name = "TextBox1"
-        TextBox1.Size = New Size(150, 31)
-        TextBox1.TabIndex = 12
+        txtTraveler1.Location = New Point(1287, 207)
+        txtTraveler1.Margin = New Padding(5)
+        txtTraveler1.Name = "txtTraveler1"
+        txtTraveler1.Size = New Size(252, 47)
+        txtTraveler1.TabIndex = 12
+        ' 
+        ' txtTraveler1Init
+        ' 
+        txtTraveler1Init.Location = New Point(1287, 431)
+        txtTraveler1Init.Margin = New Padding(5)
+        txtTraveler1Init.Name = "txtTraveler1Init"
+        txtTraveler1Init.Size = New Size(252, 47)
+        txtTraveler1Init.TabIndex = 13
+        ' 
+        ' lblTraveler1
+        ' 
+        lblTraveler1.AutoSize = True
+        lblTraveler1.Location = New Point(1340, 128)
+        lblTraveler1.Margin = New Padding(5, 0, 5, 0)
+        lblTraveler1.Name = "lblTraveler1"
+        lblTraveler1.Size = New Size(143, 41)
+        lblTraveler1.TabIndex = 14
+        lblTraveler1.Text = "Traveler 1"
+        ' 
+        ' lblTraverl1Ininit
+        ' 
+        lblTraverl1Ininit.AutoSize = True
+        lblTraverl1Ininit.Location = New Point(1304, 358)
+        lblTraverl1Ininit.Margin = New Padding(5, 0, 5, 0)
+        lblTraverl1Ininit.Name = "lblTraverl1Ininit"
+        lblTraverl1Ininit.Size = New Size(102, 41)
+        lblTraverl1Ininit.TabIndex = 15
+        lblTraverl1Ininit.Text = "Initials"
+        ' 
+        ' lblTraveler2Ininit
+        ' 
+        lblTraveler2Ininit.AutoSize = True
+        lblTraveler2Ininit.Location = New Point(1640, 343)
+        lblTraveler2Ininit.Margin = New Padding(5, 0, 5, 0)
+        lblTraveler2Ininit.Name = "lblTraveler2Ininit"
+        lblTraveler2Ininit.Size = New Size(102, 41)
+        lblTraveler2Ininit.TabIndex = 19
+        lblTraveler2Ininit.Text = "Initials"
+        ' 
+        ' lblTraveler1Ininit
+        ' 
+        lblTraveler1Ininit.AutoSize = True
+        lblTraveler1Ininit.Location = New Point(1676, 113)
+        lblTraveler1Ininit.Margin = New Padding(5, 0, 5, 0)
+        lblTraveler1Ininit.Name = "lblTraveler1Ininit"
+        lblTraveler1Ininit.Size = New Size(143, 41)
+        lblTraveler1Ininit.TabIndex = 18
+        lblTraveler1Ininit.Text = "Traveler 2"
+        ' 
+        ' txtTraveler2Init
+        ' 
+        txtTraveler2Init.Location = New Point(1624, 417)
+        txtTraveler2Init.Margin = New Padding(5)
+        txtTraveler2Init.Name = "txtTraveler2Init"
+        txtTraveler2Init.Size = New Size(252, 47)
+        txtTraveler2Init.TabIndex = 17
+        ' 
+        ' txtTraveler2
+        ' 
+        txtTraveler2.Location = New Point(1624, 192)
+        txtTraveler2.Margin = New Padding(5)
+        txtTraveler2.Name = "txtTraveler2"
+        txtTraveler2.Size = New Size(252, 47)
+        txtTraveler2.TabIndex = 16
         ' 
         ' frmEditTrip
         ' 
-        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleDimensions = New SizeF(17F, 41F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(1452, 480)
-        Controls.Add(TextBox1)
+        ClientSize = New Size(2468, 787)
+        Controls.Add(lblTraveler2Ininit)
+        Controls.Add(lblTraveler1Ininit)
+        Controls.Add(txtTraveler2Init)
+        Controls.Add(txtTraveler2)
+        Controls.Add(lblTraverl1Ininit)
+        Controls.Add(lblTraveler1)
+        Controls.Add(txtTraveler1Init)
+        Controls.Add(txtTraveler1)
         Controls.Add(btnDeleteTrip)
         Controls.Add(btnCancel)
         Controls.Add(Label4)
@@ -164,7 +232,6 @@ Partial Class frmEditTrip
         Controls.Add(txtTripDate)
         Controls.Add(btnEdit)
         Controls.Add(cbTripName)
-        Margin = New Padding(2)
         Name = "frmEditTrip"
         Text = "Edit Trip"
         ResumeLayout(False)
@@ -182,5 +249,12 @@ Partial Class frmEditTrip
     Friend WithEvents Label4 As Label
     Friend WithEvents btnCancel As Button
     Friend WithEvents btnDeleteTrip As Button
-    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents txtTraveler1 As TextBox
+    Friend WithEvents txtTraveler1Init As TextBox
+    Friend WithEvents lblTraveler1 As Label
+    Friend WithEvents lblTraverl1Ininit As Label
+    Friend WithEvents lblTraveler2Ininit As Label
+    Friend WithEvents lblTraveler1Ininit As Label
+    Friend WithEvents txtTraveler2Init As TextBox
+    Friend WithEvents txtTraveler2 As TextBox
 End Class
--- frmEditTrip.vb
+++ frmEditTrip.vb
@@ -10,44 +10,83 @@
             cbTripName.ValueMember = "TripID"
             cbTripName.SelectedIndex = -1
         End Using
+
+        ' Clear all input fields
+        txtTripDate.Clear()
+        txtTripDestination.Clear()
+        txtTripDetails.Clear()
+        txtTraveler1.Clear()
+        txtTraveler1Init.Clear()
+        txtTraveler2.Clear()
+        txtTraveler2Init.Clear()
     End Sub
     Private Sub cbTripName_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbTripName.SelectedIndexChanged
-        If cbTripName.SelectedIndex < 0 OrElse cbTripName.SelectedItem Is Nothing Then
-            txtTripDate.Clear()
-            txtTripDestination.Clear()
-            txtTripDetails.Clear()
-            selectedTripID = -1
-            Return
-        End If
+        If cbTripName.SelectedIndex = -1 Then Exit Sub
 
         Dim selectedTrip = TryCast(cbTripName.SelectedItem, Trip)
-        If selectedTrip IsNot Nothing Then
-            selectedTripID = selectedTrip.TripID
-            txtTripDate.Text = selectedTrip.TripDate
-            txtTripDestination.Text = selectedTrip.TripDestination
-            txtTripDetails.Text = selectedTrip.TripDetails
-        End If
+        If selectedTrip Is Nothing Then Exit Sub
+
+        txtTripDate.Text = selectedTrip.TripDate
+        txtTripDestination.Text = selectedTrip.TripDestination
+        txtTripDetails.Text = selectedTrip.TripDetails
+
+        Using db As New TravelManagerContext()
+            Dim details = db.TripDetail.Where(Function(td) td.TripID = selectedTrip.TripID).ToList()
+            If details.Count >= 2 Then
+                Dim traveler1 = db.Traveler.FirstOrDefault(Function(t) t.TravelerID = details(0).TravelerID)
+                Dim traveler2 = db.Traveler.FirstOrDefault(Function(t) t.TravelerID = details(1).TravelerID)
+
+                If traveler1 IsNot Nothing Then
+                    txtTraveler1.Text = traveler1.Name
+                    txtTraveler1Init.Text = traveler1.Initials
+                End If
+
+                If traveler2 IsNot Nothing Then
+                    txtTraveler2.Text = traveler2.Name
+                    txtTraveler2Init.Text = traveler2.Initials
+                End If
+            End If
+        End Using
     End Sub
+
 
 
 
     Private Sub btnEdit_Click(sender As Object, e As EventArgs) Handles btnEdit.Click
-        If selectedTripID = -1 Then
+        If cbTripName.SelectedIndex = -1 Then
             MessageBox.Show("Please select a trip to edit.", "Validation", MessageBoxButtons.OK, MessageBoxIcon.Warning)
             Return
         End If
 
-
-
-
         Try
+            Dim selectedTrip = TryCast(cbTripName.SelectedItem, Trip)
+            If selectedTrip Is Nothing Then Return
+
             Using db As New TravelManagerContext()
-                Dim trip = db.Trip.FirstOrDefault(Function(t) t.TripID = selectedTripID)
+                Dim trip = db.Trip.FirstOrDefault(Function(t) t.TripID = selectedTrip.TripID)
                 If trip IsNot Nothing Then
                     trip.TripName = cbTripName.Text.Trim()
                     trip.TripDate = txtTripDate.Text.Trim()
                     trip.TripDestination = txtTripDestination.Text.Trim()
                     trip.TripDetails = txtTripDetails.Text.Trim()
+
+                    Dim details = db.TripDetail.Where(Function(td) td.TripID = trip.TripID).ToList()
+                    If details.Count >= 2 Then
+                        Dim traveler1Name = txtTraveler1.Text.Trim()
+                        Dim traveler2Name = txtTraveler2.Text.Trim()
+
+                        Dim traveler1 = db.Traveler.FirstOrDefault(Function(t) t.Name = traveler1Name)
+                        Dim traveler2 = db.Traveler.FirstOrDefault(Function(t) t.Name = traveler2Name)
+
+                        If traveler1 IsNot Nothing Then
+                            details(0).TravelerID = traveler1.TravelerID
+                        End If
+
+                        If traveler2 IsNot Nothing Then
+                            details(1).TravelerID = traveler2.TravelerID
+                        End If
+                    End If
+
                     db.SaveChanges()
                     MessageBox.Show("Trip updated successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
                     Me.Close()
@@ -60,6 +99,8 @@
             MessageBox.Show(msg, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
         End Try
     End Sub
+
+
 
     Private Sub btnCancel_Click(sender As Object, e As EventArgs) Handles btnCancel.Click
         Dim confirm = MessageBox.Show(
